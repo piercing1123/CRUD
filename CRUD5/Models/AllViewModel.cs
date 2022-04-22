38390852b8a812301884a828aa729c9c0a704348
piercing <sh881123@gmail.com>
2022-04-22 20:53:27 +0800

    three

--- CRUD5/Models/AllViewModel.cs
+++ CRUD5/Models/AllViewModel.cs
@@ -7,17 +7,18 @@ namespace CRUD5.Models
 {
     public class AllViewModel
     {
-        public tcustomer customer { get; set; }
+     /* public tcustomer customer { get; set; }
         public temployee employee { get; set; }
-        public tproduct proruct { get; set; }
-        public tpurchase purchase { get; set; }
+        public tproduct proruct { get; set; }  */
+        public tpurchase purchase { get; set; } //取值
         public tsales sales { get; set; }
+
         public IEnumerable<tcustomer> vcustomer { get; set; }
         public IEnumerable<temployee> vemployee { get; set; }
         public IEnumerable<tproduct> vproduct { get; set; }
         public IEnumerable<tpurchase> vpurchase { get; set; }
         public IEnumerable<tsales> vsales { get; set; }
-        public AllViewModel()
+        public AllViewModel()  //讓View使用多個Model
         {
             dbcustomerEntities cdb = new dbcustomerEntities();
             this.vcustomer = cdb.tcustomer.ToList();
